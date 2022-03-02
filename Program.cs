428e1b2772b58ade20e67cadf5c5257479de5587
Viney <vineykhaneja999@gmail.com>
2022-03-02 14:10:28 +0530

    [Viney] FEATURE ADDED: At least ine upperCase Character in Password

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace User_Registration_Regex_Unit_Testing
             string[] checkingLastNames = { "Khaneja", "khan", "So", "Madaan" };
             string[] checkingEMails = { "abc@bl.co", "abc.xyz@bl.co", "abc.xyz@bl.co.in", "abc.@bl.co" };
             string[] checkingNo = { "91 7206594149", "917206594149", "910720659414", "9172065941491" };
-            string[] checkingPassword = { "222hvhvA2626", "9172A06594149", "910720659414", "91Ab0659c1491" };
+            string[] checkingPassword = { "A222hvhv2626", "917206594149A", "910720659414", "91Ab0659c1491" };
             userRegistrationPattern.ValidateFirstName(checkingFirstNames);
             userRegistrationPattern.ValidatelastName(checkingLastNames);
             userRegistrationPattern.ValidateeMail(checkingEMails);

bf6c48eea870dd02d615e472ba40a4a2c4a6d654
Viney <vineykhaneja999@gmail.com>
2022-03-02 19:59:25 +0530

    [Viney] FEATURE ADDED: Exact one special Character in Password

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace User_Registration_Regex_Unit_Testing
+namespace User_Registration_Regex
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -14,7 +14,7 @@ namespace User_Registration_Regex_Unit_Testing
             string[] checkingLastNames = { "Khaneja", "khan", "So", "Madaan" };
             string[] checkingEMails = { "abc@bl.co", "abc.xyz@bl.co", "abc.xyz@bl.co.in", "abc.@bl.co" };
             string[] checkingNo = { "91 7206594149", "917206594149", "910720659414", "9172065941491" };
-            string[] checkingPassword = { "Abdvhvhvsbs", "917206594149A", "vvvccbhcbh8", "hcvxhvzA8" };
+            string[] checkingPassword = { "bdvhvhv8sBbs", "91720659414@9A", "vvvccbhcbh8", "hcvxhvzA8" };
             userRegistrationPattern.ValidateFirstName(checkingFirstNames);
             userRegistrationPattern.ValidatelastName(checkingLastNames);
             userRegistrationPattern.ValidateeMail(checkingEMails);

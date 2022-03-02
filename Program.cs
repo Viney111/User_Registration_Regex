dcdc3d15785708d7d12898124dd88616354fd34e
Viney <vineykhaneja999@gmail.com>
2022-03-02 11:43:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace User_Registration_Regex_Unit_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UserRegistrationPattern userRegistrationPattern = new UserRegistrationPattern();
+            string[] checkingFirstNames = {  };
+            userRegistrationPattern.ValidateFirstName(checkingFirstNames);
+        }
+    }
+}
---
+++ UserRegistrationPattern.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace User_Registration_Regex_Unit_Testing
+{
+    internal class UserRegistrationPattern
+    {
+        public static string name = "";
+
+        public void ValidateFirstName(string[] names)
+        {
+            foreach (string n in names)
+            {
+                string result = Regex.IsMatch(n, name) ? $"{n} entered is valid" : $"{n} entered is Invalid";
+
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}

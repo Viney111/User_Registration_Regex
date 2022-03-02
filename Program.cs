40f12ffa07aff7274b7dbf96e58beec24b2bb85c
Viney <vineykhaneja999@gmail.com>
2022-03-02 13:59:06 +0530

    [Viney] FEATURE ADDED: Password with at least 8 characters

--- Program.cs
+++ Program.cs
@@ -14,10 +14,12 @@ namespace User_Registration_Regex_Unit_Testing
             string[] checkingLastNames = { "Khaneja", "khan", "So", "Madaan" };
             string[] checkingEMails = { "abc@bl.co", "abc.xyz@bl.co", "abc.xyz@bl.co.in", "abc.@bl.co" };
             string[] checkingNo = { "91 7206594149", "917206594149", "910720659414", "9172065941491" };
+            string[] checkingPassword = { "222", "917206594149", "910720659414", "91Ab0659c1491" };
             userRegistrationPattern.ValidateFirstName(checkingFirstNames);
             userRegistrationPattern.ValidatelastName(checkingLastNames);
             userRegistrationPattern.ValidateeMail(checkingEMails);
             userRegistrationPattern.ValidateMobileNo(checkingNo);
+            userRegistrationPattern.ValidatePassword(checkingPassword);
         }
     }
 }
